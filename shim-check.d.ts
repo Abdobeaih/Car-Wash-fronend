a7d6dda9efa61273a36a2e636d509f57daf889dc
Abdobeaih <abdobeaih43@gmail.com>
2026-09-09 10:56:05 +0200

    Refactor code structure for improved readability and maintainability

--- shim-check.d.ts
+++ shim-check.d.ts
@@ -1,5 +1,21 @@
 declare module '*.css';
 declare module '*.svg' {
-  const content: string;
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+declare module '*.png' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+declare module '*.jpg' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+declare module '*.jpeg' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+declare module '*.webp' {
+  const content: import('next/image').StaticImageData;
   export default content;
 }
